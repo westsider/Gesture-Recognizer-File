1d486dbbeaed1bcb5ecea4d6ee559272240bfb50
Warren Hansen <whansen1@mac.com>
2018-01-10 22:55:29 -0800

    initial commit

--- Gesture Recognizer File/ViewController.swift
+++ Gesture Recognizer File/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fileImageView: UIImageView!
+    
+    @IBOutlet weak var trashImageView: UIImageView!
+    
+    var fileViewOrigin: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addPanGesture(view: fileImageView)
+        fileViewOrigin = fileImageView.frame.origin
+        view.bringSubview(toFront: fileImageView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addPanGesture(view: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
     }
 
+    @objc func handlePan(sender:UIPanGestureRecognizer) {
+        
+        let fileView = sender.view!
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            
+            fileView.center = CGPoint(x: fileView.center.x + translation.x, y: fileView.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in: view)
+        case .ended:
+            
+            if fileView.frame.intersects(trashImageView.frame) {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.fileImageView.alpha = 0.0
+                })
+                
+            } else {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.fileImageView.frame.origin = self.fileViewOrigin
+                })
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    func 
 
 }
 
